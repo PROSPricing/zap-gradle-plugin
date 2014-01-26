3bcbc38a1c20398d186cd7cc56d5a45a62f7991a
Adam Roberts <ARoberts@Alum.RIT.edu>
2014-01-26 12:26:23 -0600

    Improved the task execution ordering and added unit tests for the plugin

--- src/main/groovy/com/pros/gradle/ZapPlugin.groovy
+++ src/main/groovy/com/pros/gradle/ZapPlugin.groovy
@@ -20,23 +20,28 @@ class ZapPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.extensions.create("zapConfig", ZapPluginExtension)
 
-        ZapStart zapStart = target.getTasks().create('zapStart', ZapStart.class) {
+        target.getTasks().create('zapStart', ZapStart.class) {
             description = 'Starts the ZAP daemon. You must set the extension properties zap.jarPath and zap.proxyPort to the ZAP jar file location and the ZAP proxy port.'
-        }
-
-        target.getTasks().create('zapActiveScan', ZapActiveScan.class) {
-            description = 'Runs the ZAP active scanner against zap.applicationUrl. It is recommended that this be done after any automated tests have completed so that the proxy is aware of those URLs.'
-            dependsOn zapStart
-        }
-
-        target.getTasks().create('zapReport', ZapReport.class) {
-            description = 'Generates a report with the current ZAP alerts for applicationUrl at reportOutputPath with type remoteFormat (HTML, JSON, or XML)'
-            dependsOn zapStart
+            finalizedBy 'zapStop'
         }
 
         target.getTasks().create('zapStop', ZapStop) {
             description = 'Stops the ZAP server ONLY if it has been started during this gradle process. Otherwise does nothing'
-            mustRunAfter zapStart
+            mustRunAfter project.tasks.zapStart
+            mustRunAfter 'zapActiveScan'
+            mustRunAfter 'zapReport'
+        }
+
+        target.getTasks().create('zapActiveScan', ZapActiveScan.class) {
+            description = 'Runs the ZAP active scanner against zap.applicationUrl. It is recommended that this be done after any automated tests have completed so that the proxy is aware of those URLs.'
+            dependsOn project.tasks.zapStart
+            finalizedBy project.tasks.zapStop
+        }
+
+        target.getTasks().create('zapReport', ZapReport.class) {
+            description = 'Generates a report with the current ZAP alerts for applicationUrl at reportOutputPath with type remoteFormat (HTML, JSON, or XML)'
+            dependsOn project.tasks.zapStart
+            finalizedBy project.tasks.zapStop
         }
     }
 }
